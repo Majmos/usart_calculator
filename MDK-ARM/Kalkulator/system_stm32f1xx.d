a1354e95bf7bfd5bef7b66390014f77d532b75c7
Marcel Guzik <mguzik555@gmail.com>
2021-06-04 18:31:52 +0200

    added loading numbers via uart

--- MDK-ARM/Kalkulator/system_stm32f1xx.d
+++ MDK-ARM/Kalkulator/system_stm32f1xx.d
@@ -2,7 +2,7 @@ kalkulator\system_stm32f1xx.o: ../Core/Src/system_stm32f1xx.c
 kalkulator\system_stm32f1xx.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
 kalkulator\system_stm32f1xx.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h
 kalkulator\system_stm32f1xx.o: ../Drivers/CMSIS/Include/core_cm3.h
-kalkulator\system_stm32f1xx.o: C:\Programy\Keil\ARM\ARMCC\bin\..\include\stdint.h
+kalkulator\system_stm32f1xx.o: C:\Keil\ARM\ARMCC\bin\..\include\stdint.h
 kalkulator\system_stm32f1xx.o: ../Drivers/CMSIS/Include/cmsis_version.h
 kalkulator\system_stm32f1xx.o: ../Drivers/CMSIS/Include/cmsis_compiler.h
 kalkulator\system_stm32f1xx.o: ../Drivers/CMSIS/Include/cmsis_armcc.h
@@ -13,7 +13,7 @@ kalkulator\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal
 kalkulator\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h
 kalkulator\system_stm32f1xx.o: ../Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h
 kalkulator\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h
-kalkulator\system_stm32f1xx.o: C:\Programy\Keil\ARM\ARMCC\bin\..\include\stddef.h
+kalkulator\system_stm32f1xx.o: C:\Keil\ARM\ARMCC\bin\..\include\stddef.h
 kalkulator\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h
 kalkulator\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h
 kalkulator\system_stm32f1xx.o: ../Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h
